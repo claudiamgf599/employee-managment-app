d5e9988115af6287212722baffbe57dd39d46ab9
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-11 11:00:02 -0500

    se crean los componentes de la aplicacion y se comienza el desarrollo del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        // Default route
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        // Alfter the login, the user will be redirected to the layout component
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'employee',
+                component: EmployeeComponent
+            }
+        ]
+    }
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class LoginComponent {
+
+}
