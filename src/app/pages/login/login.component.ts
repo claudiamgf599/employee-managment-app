d177c67f805da2aff67c0a683dea899da2148b00
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-12 11:53:53 -0500

    Implementacion del login y del layout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   encapsulation: ViewEncapsulation.Emulated
 })
 export class LoginComponent {
+  loginObj: any = {
+    "userName": "",
+    "password": ""
+  };
 
+  http = inject(HttpClient); // ya no se requiere el constructor para esto
+  router = inject(Router);
+
+  onLogin() {
+    debugger;
+    this.http.post('https://projectapi.gerasim.in/api/EmployeeManagement/login',
+      this.loginObj).subscribe((res: any) => {
+        debugger;
+        if (res.result) {
+          localStorage.setItem('employeeApp', JSON.stringify(res.data));
+          debugger;
+          this.router.navigateByUrl('dashboard');
+        } else {
+          alert(res.message);
+        }
+      })
+  }
 }
