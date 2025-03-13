6f5df9d6f338af1ca4d86df67f8c2a6206918173
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-13 08:37:01 -0500

    se agrega barra de progreso en login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,10 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ProgressBarComponent } from "../../reusable/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, ProgressBarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   encapsulation: ViewEncapsulation.Emulated
@@ -16,6 +17,8 @@ export class LoginComponent {
     "password": ""
   };
 
+  progressValue: number = 0;
+
   http = inject(HttpClient); // ya no se requiere el constructor para esto
   router = inject(Router);
 
@@ -23,14 +26,19 @@ export class LoginComponent {
     debugger;
     this.http.post('https://projectapi.gerasim.in/api/EmployeeManagement/login',
       this.loginObj).subscribe((res: any) => {
-        debugger;
-        if (res.result) {
-          localStorage.setItem('employeeApp', JSON.stringify(res.data));
-          debugger;
-          this.router.navigateByUrl('dashboard');
-        } else {
-          alert(res.message);
-        }
+        this.progressValue = 50;
+        setTimeout(() => {
+          if (res.result) {
+            localStorage.setItem('employeeApp', JSON.stringify(res.data));
+            debugger;
+            this.progressValue = 100;
+            this.router.navigateByUrl('dashboard');
+          } else {
+            debugger;
+            alert(res.message);
+            this.progressValue = 0;
+          }
+        }, 1000); // Delay of 1 second
       })
   }
 }
